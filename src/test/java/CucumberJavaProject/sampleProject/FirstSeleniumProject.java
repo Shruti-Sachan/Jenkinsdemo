2d74dd071879cc0c8a5d13bfef4b12dba2dc70a9
Shruti-Sachan <166524072+Shruti-Sachan@users.noreply.github.com>
2024-04-10 16:53:21 +0530

    Update FirstSeleniumProject.java

--- src/test/java/CucumberJavaProject/sampleProject/FirstSeleniumProject.java
+++ src/test/java/CucumberJavaProject/sampleProject/FirstSeleniumProject.java
@@ -1,36 +1,42 @@
 package CucumberJavaProject.sampleProject;
-
+ 
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+ 
 public class FirstSeleniumProject {
     static WebDriver driver;
-
-    @Test
-    public void testSearchText() {
-        System.out.println("Running testSearchText");
-        initializeWebdriver();
-        navigateToGoogleSearchPage();
-        
-    }
-
-    // Initialize Chrome
+ 
+    @BeforeTest
     public static void initializeWebdriver() {
         // Set the path for chromeDriver
     	System.setProperty(" webdriver.chrome.driver",
     			"C:\\Users\\shrutisachan\\eclipse\\java-2022-12\\eclipse\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
     			driver = new ChromeDriver();
     }
-
-    // Navigate to Google home page
+ 
+    @Test
     public static void navigateToGoogleSearchPage() {
-        driver.get("https://www.amazon.com/");
-        driver.quit();
+    	try {
+    		Thread.sleep(1000);
+            driver.get("https://www.amazon.com/");	
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
     }
-
+    @AfterTest
+    public static void afterTest() {
+    	try {
+    		driver.quit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    }
+ 
     
-
+ 
 }
